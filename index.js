65bb0c2afbf75b43f36fa52ca2a6bb08bf8e5652
Daniel Papert <dpapert@gmail.com>
2022-09-21 16:21:02 -0400

    Added requires and packages for code.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// TODO: Include packages needed for this application
+// BEGIN README GENERATOR CODE
+// TODO: Include packages needed for this application through requires
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [];
